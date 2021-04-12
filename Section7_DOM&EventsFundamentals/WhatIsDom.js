58bd1dfd2e9024c96846aac6347565801a7f9536
kangandhi <Kanchan@benjamin.co.jp>
2021-04-12 19:28:35 +0900

    sixth upload

---
+++ Section7_DOM&EventsFundamentals/WhatIsDom.js
@@ -0,0 +1,7 @@
+//DOM : Document Object model 
+// Structured representation of HTML documents, allows javascript to access HTML elements & styles to manipulate them.
+
+
+// Manipulate - change text, HTML Attributes and even CSS styles. 
+
+
